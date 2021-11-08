44036e5c50aabc18104e4129e5c5ca66c4700c0d
Ayush <ayushsenapati123@gmail.com>
2021-11-08 21:37:21 +0530

    Added materialize css and js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, {useEffect} from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  })
+
   return (
     <div className="App">
-      My App
+      My Web App
     </div>
   );
 }
