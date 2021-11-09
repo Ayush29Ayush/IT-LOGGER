a9ec151409bf9e872be579126ce531916c022745
Ayush <ayushsenapati123@gmail.com>
2021-11-09 14:05:42 +0530

    Added id info and last updated

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import Moment from "react-moment";
+import PropTypes from "prop-types";
 
-const LogItem = ({log}) => {
-    return (
-        <li className="collection-item">
-            <div>
-                <a href="">{log.message}</a>
-            </div>
-        </li>
-    )
-}
+const LogItem = ({ log }) => {
+  return (
+    <li className="collection-item">
+      <div>
+        <a
+          href="#edit-log-modal"
+          className={`modal-trigger ${
+            log.attention ? "red-text" : "blue-text"
+          }`}
+        >
+          {log.message}
+        </a>
+        <br />
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id}</span> last updated by{" "}
+          <span className="black-text">{log.tech}</span>
+        </span>
+      </div>
+    </li>
+  );
+};
 
 LogItem.propTypes = {
-    log: PropTypes.object.isRequired,
-}
+  log: PropTypes.object.isRequired,
+};
 
-export default LogItem
+export default LogItem;
