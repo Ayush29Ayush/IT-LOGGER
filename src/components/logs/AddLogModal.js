a4cc55ff2aff6c0d968ebc7ef3c80bac0c8e39dd
Ayush <ayushsenapati123@gmail.com>
2021-11-09 20:04:05 +0530

    AddLogModal

---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+const AddLogModal = () => {
+    const [message, setMessage] = useState('');
+    const [attention, setAttention] = useState(false);
+    const [tech, setTech] = useState('');
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddLogModal
