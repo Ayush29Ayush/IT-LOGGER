db0dd0f772687a55fbc28560da7cd79b39e5d446
Ayush <ayushsenapati123@gmail.com>
2021-11-09 17:11:18 +0530

    Added date and delete

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -17,8 +17,12 @@ const LogItem = ({ log }) => {
         <br />
         <span className="grey-text">
           <span className="black-text">ID #{log.id}</span> last updated by{" "}
-          <span className="black-text">{log.tech}</span>
+          <span className="black-text">{log.tech}</span> on{" "}
+          <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
+        <a href="#!" className="secondary-content">
+          <i className="material-icons grey-text">delete</i>
+        </a>
       </div>
     </li>
   );
