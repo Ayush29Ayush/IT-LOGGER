397e441bc3bd084f998968f45adbe06e50f453e7
Ayush <ayushsenapati123@gmail.com>
2021-11-10 16:00:17 +0530

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
-        <EditLogModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
