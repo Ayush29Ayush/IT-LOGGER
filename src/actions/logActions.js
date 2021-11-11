5317644e3b2ec5ad72436ddb34dd6a207d314a68
Ayush <ayushsenapati123@gmail.com>
2021-11-11 15:22:40 +0530

    getLogs modified

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,7 +1,24 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
-export const getLogs = () => {
-  return async (dispatch) => {
+//! For getLogs
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+
+//     const res = await fetch("/logs");
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+//! For getLogs plus error handling
+// this is the short hand notation of writing return async (dispatch) => {
+export const getLogs = () => async (dispatch) => {
+  try {
     setLoading();
 
     const res = await fetch("/logs");
@@ -11,7 +28,12 @@ export const getLogs = () => {
       type: GET_LOGS,
       payload: data,
     });
-  };
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
 };
 
 // Set loading to true
