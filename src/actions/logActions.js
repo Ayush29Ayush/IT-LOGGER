1af08627a2917843bc6b003d8538d17e9d7c006c
Ayush <ayushsenapati123@gmail.com>
2021-11-11 14:04:58 +0530

    configured getLogs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,12 +1,22 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR} from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
 export const getLogs = () => {
-    
-}
+  return async (dispatch) => {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  };
+};
 
 // Set loading to true
 export const setLoading = () => {
-    return {
-        type: SET_LOADING
-    }
-}
+  return {
+    type: SET_LOADING,
+  };
+};
