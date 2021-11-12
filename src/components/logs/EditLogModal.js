7a50ed94008332683a88a96176a18aea9406f0df
Ayush <ayushsenapati123@gmail.com>
2021-11-12 12:48:17 +0530

    Update Logs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -21,7 +21,17 @@ const EditLogModal = ({ current, updateLog }) => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and tech" });
     } else {
-      console.log(message, tech, attention);
+      // console.log(message, tech, attention);
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      }
+
+      updateLog(updLog);
+      M.toast({html: `Log updated by ${tech}`})
 
       // Clear Fields
       setMessage("");
