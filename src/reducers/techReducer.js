d1c28da013178857e6475a92b076c7012881689f
Ayush <ayushsenapati123@gmail.com>
2021-11-13 17:02:48 +0530

    add technician reducer

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils";
 import {
   GET_TECHS,
   ADD_TECH,
@@ -20,11 +21,24 @@ export default (state = initialState, action) => {
         techs: action.payload,
         loading: false,
       };
+    case ADD_TECH:
+      return {
+        ...state,
+        techs: [...state.techs, action.payload],
+        loading: false
+      };
     case SET_LOADING:
       return {
         ...state,
         loading: true,
       };
+    case TECHS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      }
     default:
       return state;
   }
